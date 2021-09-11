00bb56015a2c1d0700ce3117785f9e2418bfa2dc
mazenolama <44679914+mazenolama@users.noreply.github.com>
2021-09-11 15:25:11 +0200

    footer

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Webslesson <?php echo date('Y'); ?></span>
+                        <span>Copyright &copy; Mazen Olama <?php echo date('Y'); ?></span>
                     </div>
                 </div>
             </footer>
