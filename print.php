52b33d7392d4795edaa88171e9f796f3edd506ea
mazenolama <44679914+mazenolama@users.noreply.github.com>
2021-09-11 21:49:57 +0200

    access

--- print.php
+++ print.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+ header('Content-Type: application/json');
 //print.php
 
 require_once('class/pdf.php');
--- user_action.php
+++ user_action.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+ header('Content-Type: application/json');
 //user_action.php
 
 include('rms.php');
